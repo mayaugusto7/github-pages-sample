8540b3e243d4e5f747a2177c43971da3e06d2df4
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2017-10-25 12:26:44 -0200

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var $paragraph = document.querySelector('p')
+var $paragraph = document.querySelector('p');
 
-$paragraph.textContent = 'Conteúdo inserido via javascript'
+$paragraph.textContent = 'Conteúdo inserido via javascript';
